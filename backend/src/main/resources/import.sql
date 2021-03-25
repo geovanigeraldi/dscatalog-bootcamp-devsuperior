054a2ac8bdf2670529399efd815d5c83e9191d00
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-03-25 16:42:06 -0300

    product entity, script sql

--- backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into tb_category (name, created_At) values ('Books', now());
-insert into tb_category (name, created_At) values ('Electronics', now());
-insert into tb_category (name, created_At) values ('Computers', now());
