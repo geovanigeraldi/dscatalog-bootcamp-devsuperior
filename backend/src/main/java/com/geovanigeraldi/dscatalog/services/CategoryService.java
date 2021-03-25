504c60366acbb605d6c269010e1629c3befd3255
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-03-25 15:37:51 -0300

    delete category

--- backend/src/main/java/com/geovanigeraldi/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/geovanigeraldi/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.geovanigeraldi.dscatalog.dto.CategoryDTO;
 import com.geovanigeraldi.dscatalog.entities.Category;
 import com.geovanigeraldi.dscatalog.repositories.CategoryRepository;
+import com.geovanigeraldi.dscatalog.services.exceptions.DataBaseException;
 import com.geovanigeraldi.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service // registra como um componente gerenciado pelo spring
@@ -43,7 +46,7 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional
 	public CategoryDTO update(Long id, CategoryDTO dto) {
 		try {
 			Category entity = repository.getOne(id);
@@ -52,7 +55,20 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		}
 		catch(EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id not found " + id);
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+	}
+
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id not found: " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation");
 		}
 	}
 }
